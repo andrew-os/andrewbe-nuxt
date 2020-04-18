0cc820154b1f424ced0b5f5c97c01601291e3ea0
Andrew Sleigh <andrewsleigh@akauk.com>
2020-04-18 06:13:33 +0100

    project item component built out with props coming in, this component imported into project page vue

--- components/Project-item.vue
+++ components/Project-item.vue
@@ -1,62 +1,70 @@
- <script>
-//     Vue.component('project-item', {
-//         template: `
-//             <li>
-//                 {title}
-//             </li>
-//         `,
-//         props: ['title']
-//     })
+<template>
+    <div class="pl-6">
+        <li v-for="project in projects" :key="project.title">
+           <a :href="project.url" target="_blank">
+               {{project.title}}
+            </a> 
+        </li>
 
-//         data: {
-//             projects:[
-//                 {
-//                     title: 'Punchdrunk',
-//                     url: 'https://punchdrunk.org.uk/',
-//                 },
-//                 {
-//                     title: 'Edge NYC',
-//                     url: 'https://edgenyc.com/',
-//                 },
-//                 {
-//                     title: 'Lumiere',
-//                     url: '',
-//                 },
-//                 {
-//                     title: 'New York Perrenial Garden',
-//                     url: '',
-//                 },
-//                 {
-//                     title: 'Fairfield Halls',
-//                     url: '',
-//                 },
-//                 {
-//                     title: 'Harry Potter - uk/us/de/au',
-//                     url: '',
-//                 },
-//                 {
-//                     title: 'The View from the Shard',
-//                     url: '',
-//                 },
-//                 {
-//                     title: 'Skatepal',
-//                     url: '',
-//                 },
-//                 {
-//                     title: 'Eightwire',
-//                     url: '',
-//                 },
-//                 {
-//                     title: 'Lockley Concrete',
-//                     url: '',
-//                 },
-//                 {
-//                     title: 'Coach Approach',
-//                     url: '',
-//                 },
-//             ]
-//         }
+    </div>
+
+</template>
+<script>
+export default{
+    data() {
+            return{
+                projects:[
+                    {
+                        title: 'Punchdrunk',
+                        url: 'https://punchdrunk.org.uk/',
+                    },
+                    {
+                        title: 'Edge NYC',
+                        url: 'https://edgenyc.com/',
+                    },
+                    {
+                        title: 'Lumiere',
+                        url: 'https://www.lumiere-festival.com/',
+                    },
+                    {
+                        title: 'New York Perrenial Garden',
+                        url: 'https://pg.nybg.org/#/',
+                    },
+                    {
+                        title: 'Fairfield Halls',
+                        url: 'https://www.fairfield.co.uk/',
+                    },
+                    {
+                        title: 'Harry Potter - uk/us/de/au',
+                        url: 'https://www.harrypottertheplay.com/',
+                    },
+                    {
+                        title: 'The View from the Shard',
+                        url: 'https://www.theviewfromtheshard.com/',
+                    },
+                    {
+                        title: 'Skatepal',
+                        url: 'http://www.skatepal.co.uk/',
+                    },
+                    {
+                        title: 'Eightwire',
+                        url: 'https://eightwire.uk/services/websites/',
+                    },
+                    {
+                        title: 'Lockley Concrete',
+                        url: 'https://lockleyconcrete.co.uk/',
+                    },
+                    {
+                        title: 'Coach Approach',
+                        url: 'https://thecoachapproach.co.uk/',
+                    },
+                ]
+            }
+           
+        }
+}
+</script>
+
 
         
 
- </script>
--- pages/projects.vue
+++ pages/projects.vue
@@ -3,10 +3,25 @@
         <h1 class="text-4xl text-txtwhite">Projects</h1>
 
         <p>Having worked as a Frontend developer for numerous I have worked on 100's of websites.</p>
-        <h2 class="text-txtwhite text-1xl">These are websites I’ve built and worked on recently that showcase different designs, features and coding methods:</h2>
+        <p class="text-txtwhite text-1xl mb-4">These are websites I’ve built and worked on recently that showcase different designs, features and coding methods:</p>
         
-
+        <projectitem></projectitem>
+        <br>
+        <br>
+        <p>This site is a Vue/Nuxt clone of my personal <a href="https://andrewbe.me" target="_blank">site</a>(built with React/Gatsby)</p>
+        <p>I've always found learning on the job a great way of wrapping my head around concepts.</p>
+       
 
     </div>
 </template>
 
+<script>
+import projectitem from '~/components/project-item.vue'
+
+export default {
+  components: {
+    projectitem,
+  }
+}
+
+</script>
